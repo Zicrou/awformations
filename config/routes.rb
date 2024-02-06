2a8a6c0f726a5368078130bd77f4fbc814b09b8b
Zicrou <aazseck@gmail..com>
2024-02-06 18:21:34 +0000

    Added footer

--- config/routes.rb
+++ config/routes.rb
@@ -16,5 +16,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-   root "products#index"
+   #root "products#index"
+   root "home#index"
 end
